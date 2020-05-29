e83778d39f0472933332605a94b651c7c3162d0c
ongspxm <ongspxm@gmail.com>
2020-05-29 19:38:46 +0800

    (style) retab

--- ftplugin/orgmode/liborgmode/agenda.py
+++ ftplugin/orgmode/liborgmode/agenda.py
@@ -12,52 +12,53 @@ u"""
 	* sorting
 """
 
+from orgmode.liborgmode.agendafilter import contains_active_date
+from orgmode.liborgmode.agendafilter import contains_active_todo
 from orgmode.liborgmode.agendafilter import filter_items
 from orgmode.liborgmode.agendafilter import is_within_week_and_active_todo
-from orgmode.liborgmode.agendafilter import contains_active_todo
-from orgmode.liborgmode.agendafilter import contains_active_date
 
 
 class AgendaManager(object):
-	u"""Simple parsing of Documents to create an agenda."""
-	# TODO Move filters in this file, they do the same thing
+    u"""Simple parsing of Documents to create an agenda."""
 
-	def __init__(self):
-		super(AgendaManager, self).__init__()
+    # TODO Move filters in this file, they do the same thing
 
-	def get_todo(self, documents):
-		u"""
-		Get the todo agenda for the given documents (list of document).
-		"""
-		filtered = []
-		for document in iter(documents):
-			# filter and return headings
-			filtered.extend(filter_items(document.all_headings(),
-								[contains_active_todo]))
-		return sorted(filtered)
+    def __init__(self):
+        super(AgendaManager, self).__init__()
 
-	def get_next_week_and_active_todo(self, documents):
-		u"""
-		Get the agenda for next week for the given documents (list of
-		document).
-		"""
-		filtered = []
-		for document in iter(documents):
-			# filter and return headings
-			filtered.extend(filter_items(document.all_headings(),
-								[is_within_week_and_active_todo]))
-		return sorted(filtered)
+    def get_todo(self, documents):
+        u"""
+        Get the todo agenda for the given documents (list of document).
+        """
+        filtered = []
+        for document in iter(documents):
+            # filter and return headings
+            filtered.extend(filter_items(document.all_headings(),
+                [contains_active_todo]))
+        return sorted(filtered)
 
-	def get_timestamped_items(self, documents):
-		u"""
-		Get all time-stamped items in a time-sorted way for the given
-		documents (list of document).
-		"""
-		filtered = []
-		for document in iter(documents):
-			# filter and return headings
-			filtered.extend(filter_items(document.all_headings(),
-								[contains_active_date]))
-		return sorted(filtered)
+    def get_next_week_and_active_todo(self, documents):
+        u"""
+        Get the agenda for next week for the given documents (list of
+        document).
+        """
+        filtered = []
+        for document in iter(documents):
+            # filter and return headings
+            filtered.extend(filter_items(document.all_headings(),
+                [is_within_week_and_active_todo]))
+        return sorted(filtered)
+
+    def get_timestamped_items(self, documents):
+        u"""
+        Get all time-stamped items in a time-sorted way for the given
+        documents (list of document).
+        """
+        filtered = []
+        for document in iter(documents):
+            # filter and return headings
+            filtered.extend(filter_items(document.all_headings(),
+                [contains_active_date]))
+        return sorted(filtered)
 
 # vim: set noexpandtab:
