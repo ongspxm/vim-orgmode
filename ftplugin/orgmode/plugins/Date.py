cbb80e9b71179889caf734fae88fb651e00f2106
ongspxm <ongspxm@gmail.com>
2020-06-07 16:05:33 +0800

    (date) handle repeated dates

--- ftplugin/orgmode/plugins/Date.py
+++ ftplugin/orgmode/plugins/Date.py
@@ -6,6 +6,7 @@ from datetime import date, datetime, timedelta
 import vim
 from orgmode._vim import ORGMODE, echom, get_user_input, insert_at_cursor
 from orgmode.menu import ActionEntry, Submenu, add_cmd_mapping_menu
+from orgmode.liborgmode.orgdate import get_orgdate
 from orgmode.py3compat.encode_compatibility import *
 from orgmode.py3compat.py_py3_string import *
 from orgmode.py3compat.unicode_compatibility import *
@@ -222,48 +223,80 @@ class Date(object):
 			return startdate
 
 	@classmethod
-	def insert_timestamp_header(self, datetype, now=False, remove=False):
-		"""
-		with headers
-		"""
+	def update_todo_timestamp(self, isdone=True):
+		""" return true if need to push to next tag """
+		datetype = "CLOSED"
+		if not isdone:
+			return self.insert_timestamp_header(datetype, remove=True)
+
+		info, _ = self.get_timestamp_header()
+
+		try:
+			# add time to scheduled
+			datetype2 = 'SCHEDULED'
+			scheduled = get_orgdate(info.get(datetype2))
+			repeater = scheduled.repeater
+			assert repeater
+
+			currdate = date.today() if repeater[0] == '.' else scheduled
+			newdate = get_orgdate('<%s Abc %s>'%(
+				self._modify_time(currdate, repeater[1:]), repeater))
+			print(newdate)
+
+			self.insert_timestamp_header(datetype2, usetime=newdate)
+			return True
+		except Exception as e:
+			print(e)
+			return self.insert_timestamp_header(datetype, usetime=False)
+
+	@classmethod
+	def get_timestamp_header(self):
+		""" info, body
+			- info is the dict of time headers
+			- body is the list of strings """
+		# will do all the checks and stuff
+		d = ORGMODE.get_document(allow_dirty=True)
+
+		info, body = {}, d.find_current_heading().body
+		try:
+			firstline = body[0]
+			if not firstline.startswith(' '):
+				parts = [f.split(': ') for f in
+					firstline.strip().replace(']', '>').split('>') if f]
+
+				for k, v in parts:
+					k = k.strip()
+					if k != k.upper():
+						raise
+
+					if v[0] == '<':
+						v += '>'
+					elif v[0] == '[':
+						v += ']'
+					else:
+						raise
+					info[k] = v
+				body.pop(0)
+		except:
+			info = {}
+
+		return info, body
+
+	@classmethod
+	def insert_timestamp_header(self, datetype, usetime=None, remove=False):
+		""" with headers,
+			- datetype = keyword (eg SCHEUDLED, CLOSED, etc)
+			- usetime = date to use, instead of tstamp
+			- remove = whether to remove the key or not, used for when DONE => TODO """
 		tstamp = ''
 		if not remove:
-			tstamp = (self.insert_timestamp(writeout=False) if not now else
-					datetime.now().strftime('[%Y-%m-%d %a %H:%M]').upper())
+			tstamp = (self.insert_timestamp(writeout=False) if usetime is None else
+					(usetime or datetime.now().strftime('[%Y-%m-%d %a %H:%M]').upper()))
 
-		if not tstamp:
+		if not tstamp and not remove:
 			return
 
-		d = ORGMODE.get_document(allow_dirty=True)
-		heading = d.find_current_heading()
-		body = heading.body
-
-		info = {}
-		if len(body):
-			firstline = body[0]
-
-			# will do all the checks and stuff
-			try:
-				if not firstline.startswith(' '):
-					parts = [f.split(': ') for f in
-						firstline.strip().replace(']', '>').split('>') if f]
-
-					for k, v in parts:
-						k = k.strip()
-						if k != k.upper():
-							raise
-
-						if v[0] == '<':
-							v += '>'
-						elif v[0] == '[':
-							v += ']'
-						else:
-							raise
-						info[k] = v
-					body.pop(0)
-			except:
-				info = {}
-
+		info, body = self.get_timestamp_header()
 		# clean up newlines at head
 		while len(body) and len(body[0].strip()) == 0:
 			body.pop(0)
@@ -280,8 +313,12 @@ class Date(object):
 		if firstline:
 			body = [firstline]+body
 
+		d = ORGMODE.get_document(allow_dirty=True)
+		heading = d.find_current_heading()
 		heading.body = (body)
 		d.write_heading(heading)
+
+		return False
 
 	@classmethod
 	def insert_timestamp_with_calendar(cls, active=True):
@@ -337,7 +374,6 @@ class Date(object):
 		if writeout:
 			insert_at_cursor(timestamp)
 
-		print(' --DONE--')
 		return timestamp
 
 	def register(self):
